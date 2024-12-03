45779cc12e05ec330b4ecff5da1aaf1e210f470b
GSacata <gsacata@gmail.com>
2024-12-02 21:03:15 -0300

    Created basic page structure

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MadreperolaComponent } from './madreperola/madreperola.component';
+import { PerolaDoLitoralComponent } from './perola-do-litoral/perola-do-litoral.component';
+import { ContatoComponent } from './contato/contato.component';
+import { BuffetComponent } from './buffet/buffet.component';
+import { AdminComponent } from './admin/admin.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HomeComponent, MadreperolaComponent, PerolaDoLitoralComponent, ContatoComponent, BuffetComponent, AdminComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
