d19d34ed8cd0ba1ee52e487c42be48d06df16eff
GSacata <gsacata@gmail.com>
2024-12-03 21:05:59 -0300

    Added header and footer components

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { PerolaDoLitoralModule } from '../perola-do-litoral/perola-do-litoral.module';
+import { HeaderComponent } from "../header/header.component";
+import { FooterComponent } from "../footer/footer.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    PerolaDoLitoralModule,
+    HeaderComponent,
+    FooterComponent
+],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
 
+export class HomeComponent {
+  constructor () {}
 }
