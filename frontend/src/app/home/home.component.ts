8ebcd3975ea7949ef246e0cce778222f5cdc871d
GSacata <gsacata@gmail.com>
2024-12-26 22:33:12 -0300

    Positioning some content

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,15 +1,11 @@
 import { Component } from '@angular/core';
 import { PerolaDoLitoralModule } from '../perola-do-litoral/perola-do-litoral.module';
-import { HeaderComponent } from "../header/header.component";
-import { FooterComponent } from "../footer/footer.component"
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     PerolaDoLitoralModule,
-    HeaderComponent,
-    FooterComponent
 ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
