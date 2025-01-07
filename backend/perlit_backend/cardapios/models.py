413a8f221849a4d3bbf6cb891a0a43e0fbca1ff7
GSacata <gsacata@gmail.com>
2025-01-06 21:47:01 -0300

    Created restaurant menu app, and menu model.

---
+++ backend/perlit_backend/cardapios/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class CardapioRestaurante(models.Model):
+    dish = models.CharField(max_length=100)
+    has_vegetarian = models.BooleanField(default=False)
+    veget_variety = models.TextField(max_length=40)
+    has_vegan = models.BooleanField(default=False)
+    vegan_variety = models.TextField(max_length=60)
+    notes = models.TextField()
+
